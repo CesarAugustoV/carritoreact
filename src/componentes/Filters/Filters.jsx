c2f607a41aa124b384ec75f089d6e79fcdecbb8f
cesaraugustovicci@icloud.com <cesaraugustovicci@icloud.com>
2023-07-12 10:25:29 -0400

    funcionando

--- src/componentes/Filters/Filters.jsx
+++ src/componentes/Filters/Filters.jsx
@@ -182,14 +182,14 @@ export function Filters({mostrarProducto}) {
                 </Transition.Root>
 
                 <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                    <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
-                        <h1 className="text-4xl font-bold tracking-tight text-gray-900">New Arrivals</h1>
+                    <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-10">
+                        <h1 className="text-4xl font-bold tracking-tight text-gray-900">Nueva temporada</h1>
 
                         <div className="flex items-center">
                             <Menu as="div" className="relative inline-block text-left">
                                 <div>
                                     <Menu.Button className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
-                                        Sort
+                                        Clasificar
                                         <ChevronDownIcon
                                             className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
                                             aria-hidden="true"
